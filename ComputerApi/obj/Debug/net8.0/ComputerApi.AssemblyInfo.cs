0d75796d5ddb8046bd93c9fc655a986c63c48568
Kuruzso <vellera@kkszki.hu>
2024-11-19 11:45:59 +0100

    Models are ready via mysql

--- ComputerApi/obj/Debug/net8.0/ComputerApi.AssemblyInfo.cs
+++ ComputerApi/obj/Debug/net8.0/ComputerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComputerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+350dfe38381afa7f510bf3588bf628049dd243b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComputerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComputerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
